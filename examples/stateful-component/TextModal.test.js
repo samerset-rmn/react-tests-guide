e401332bf31fbae28a6bd62314a4cd86e9f3adec
Roman Kosov <samerset14@gmail.com>
2021-06-10 18:19:18 +1000

    Add `stateful-component` example

---
+++ examples/stateful-component/TextModal.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import TextModal from '.';
+
+describe('TextModal comp-t', () => {
+  test('button displays the modal', () => {
+    // Содержимое компонента
+    const modalContent = 'Окно с текстом';
+
+    // Рендерим компонент с содержимым
+    render(<TextModal>{modalContent}</TextModal>);
+
+    // Находим кнопку для открытия модалки
+    const button = screen.getByRole('button', { name: /(закрыть|открыть)/i });
+    // И саму модалку (передаем параметр `hidden: true`, т.к. она изначально невидима и `getByRole` ее не найдет)
+    const modal = screen.getByRole('dialog', {
+      hidden: true
+    });
+
+    // Проверяем, что модалка невидима и содержит текст, который мы передали
+    expect(modal).not.toBeVisible();
+    expect(modal).toHaveTextContent(modalContent);
+
+    // Открываем модалку по клику на кнопку
+    userEvent.click(button);
+    // Проверяем, что она открылась
+    expect(modal).toBeVisible();
+    // Проверяем, что `aria-label` кнопки поменялся
+    expect(button).toHaveAttribute(
+      'aria-label',
+      expect.stringMatching(/закрыть/i)
+    );
+
+    // Те же действия, но в обратном порядке
+    userEvent.click(button);
+    expect(modal).not.toBeVisible();
+    expect(button).toHaveAttribute(
+      'aria-label',
+      expect.stringMatching(/открыть/i)
+    );
+  });
+});
---
+++ examples/stateful-component/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Компонент с состоянием.
+ * Отображает модальное окно, открываемое по нажатию на кнопку.
+ */
+function TextModal({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClickHandler = () => setIsOpen((state) => !state);
+
+  return (
+    <div>
+      <button
+        type='button'
+        aria-label={`${isOpen ? 'Закрыть' : 'Открыть'} модальное окно`}
+        onClick={onClickHandler}
+      />
+      <div
+        role='dialog'
+        aria-labelledby='dialogTitle'
+        style={{ display: isOpen ? 'flex' : 'none' }}
+      >
+        <div>
+          <h3 id='dialogTitle'>{children}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+TextModal.propTypes = {
+  children: PropTypes.string.isRequired
+};
+
+export default TextModal;
